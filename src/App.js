2cbcd6e241aa4e67c7721bb59ea3e909766b240d
darius29 <dariusgabriel52@gmail.com>
2024-07-22 16:56:02 +0300

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -103,9 +103,7 @@ const App = () => {
   return (
     <div className="flex flex-col items-center min-h-screen p-5 bg-gradient-to-br from-gray-100 via-gray-300 to-gray-500">
       <ToastContainer />
-      <h1 className="mb-8 text-4xl font-bold text-gray-800">
-        Datavid Cake Tracker
-      </h1>
+      <h1 className="mb-8 text-4xl font-bold text-gray-800">Cake Tracker</h1>
 
       <div className="w-full max-w-2xl">
         {editingMember ? (
